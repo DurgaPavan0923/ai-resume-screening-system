5feae79ff63f537aea4a3c31ffdd63c1474d3650
Pavan Kumar <rdpkumar2308@gmail.com>
2026-03-07 13:29:47 +0530

    Create pdf_parser.py

---
+++ src/pdf_parser.py
@@ -0,0 +1,15 @@
+from pdfminer.high_level import extract_text
+import tempfile
+
+
+def extract_text_from_pdf(uploaded_file):
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+
+        tmp_file.write(uploaded_file.read())
+
+        path = tmp_file.name
+
+    text = extract_text(path)
+
+    return text
