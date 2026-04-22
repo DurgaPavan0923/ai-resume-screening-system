c6cf6b30ca691c224d7c6ea738716176734f0c26
Pavan Kumar <rdpkumar2308@gmail.com>
2026-04-22 12:37:26 +0530

    Update job_predictor.py

--- src/job_predictor.py
+++ src/job_predictor.py
@@ -1,20 +1,4 @@
-import pickle
-from config import MODEL_PATH, VECTORIZER_PATH
-
-
-def load_model():
-    with open(MODEL_PATH, "rb") as f:
-        model = pickle.load(f)
-
-    with open(VECTORIZER_PATH, "rb") as f:
-        vectorizer = pickle.load(f)
-
-    return model, vectorizer
-
-
-def predict_role(text):
-    model, vectorizer = load_model()
+def predict_role(text, model, vectorizer):
     vec = vectorizer.transform([text])
     prediction = model.predict(vec)[0]
-
     return prediction
